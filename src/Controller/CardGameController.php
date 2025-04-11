652f7bdb1e8439e7d238bbe173313a4c401b80b4
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-04-11 17:39:25 +0200

    Create routes for card, session, session/delete, card/deck

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -12,166 +12,213 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CardGameController extends AbstractController
 {
-    #[Route("/game/pig", name: "pig_start")]
-    public function home(): Response
+    #[Route("/card", name: "card_start")]
+    public function home(SessionInterface $session): Response
     {
-        return $this->render('pig/home.html.twig');
+        $session->set("test", "funkar?");
+        return $this->render('card/home.html.twig');
     }
 
-    #[Route("/game/pig/test/roll", name: "test_roll_dice")]
-    public function testRollDice(): Response
+    #[Route("/session", name: "session")]
+    public function session(SessionInterface $session): Response
     {
-        $die = new Dice();
-
         $data = [
-            "dice" => $die->roll(),
-            "diceString" => $die->getAsString(),
+            "session" => $session->all()
         ];
 
-        return $this->render('pig/test/roll.html.twig', $data);
-    }
-    // <\d+> is a regular expression that specifies the format of the parameter. In this case, \d+ means one or more digits, so the num parameter must be a number.
-    #[Route("/game/pig/test/roll/{num<\d+>}", name: "test_roll_num_dices")]
-    public function testRollDices(int $num): Response
-    {
-        if ($num > 99) {
-            throw new \Exception("Can not roll more than 99 dices!");
-        }
-
-        $diceRoll = [];
-        for ($i = 1; $i <= $num; $i++) {
-            $die = new DiceGraphic();
-            $die->roll();
-            $diceRoll[] = $die->getAsString();
-        }
-
-        $data = [
-            "num_dices" => count($diceRoll),
-            "diceRoll" => $diceRoll,
-        ];
-
-        return $this->render('pig/test/roll_many.html.twig', $data);
+        return $this->render('session.html.twig', $data);
     }
 
-
-    #[Route("/game/pig/test/dicehand/{num<\d+>}", name: "test_dicehand")]
-    public function testDiceHand(int $num): Response
+    #[Route("/session/delete", name: "session_delete")]
+    public function sessionDelete(SessionInterface $session): Response
     {
-        if ($num > 99) {
-            throw new \Exception("Can not roll more than 99 dices!");
-        }
-
-        $hand = new DiceHand();
-        for ($i = 1; $i <= $num; $i++) {
-            if ($i % 2 === 1) {
-                $hand->add(new DiceGraphic());
-            } else {
-                $hand->add(new Dice());
-            }
-        }
-
-        $hand->roll();
-
-        $data = [
-            "num_dices" => $hand->getNumberDices(),
-            "diceRoll" => $hand->getString(),
-        ];
-
-        return $this->render('pig/test/dicehand.html.twig', $data);
-    }
-
-    #[Route("/game/pig/init", name: "pig_init_get", methods: ['GET'])]
-    public function init(): Response
-    {
-        return $this->render('pig/init.html.twig');
-    }
-
-
-    #[Route("/game/pig/init", name: "pig_init_post", methods: ['POST'])]
-    public function initCallback(
-        Request $request,
-        SessionInterface $session
-    ): Response
-    {
-        $numDice = $request->request->get('num_dices');
-
-        $hand = new DiceHand();
-        for ($i = 1; $i <= $numDice; $i++) {
-            $hand->add(new DiceGraphic());
-        }
-        $hand->roll();
-
-        $session->set("pig_dicehand", $hand);
-        $session->set("pig_dices", $numDice);
-        $session->set("pig_round", 0);
-        $session->set("pig_total", 0);
-
-        return $this->redirectToRoute('pig_play');
-    }
-
-    #[Route("/game/pig/play", name: "pig_play", methods: ['GET'])]
-    public function play(
-        SessionInterface $session
-    ): Response
-    {
-        $dicehand = $session->get("pig_dicehand");
-
-        $data = [
-            "pigDices" => $session->get("pig_dices"),
-            "pigRound" => $session->get("pig_round"),
-            "pigTotal" => $session->get("pig_total"),
-            "diceValues" => $dicehand->getString() 
-        ];
-
-        return $this->render('pig/play.html.twig', $data);
-    }
-
-    #[Route("/game/pig/roll", name: "pig_roll", methods: ['POST'])]
-    public function roll(
-        SessionInterface $session
-    ): Response
-    {
-        $hand = $session->get("pig_dicehand");
-        $hand->roll();
-
-        $roundTotal = $session->get("pig_round");
-        $round = 0;
-        $values = $hand->getValues();
-        foreach ($values as $value) {
-            if ($value === 1) {
-                $round = 0;
-                $roundTotal = 0;
-                $this->addFlash(
-                    'warning',
-                    'You got a 1 and you lost the round points!'
-                );
-                break;
-            }
-            $round += $value;
-        }
-
-        $session->set("pig_round", $roundTotal + $round);
+        $session->clear();
         
-        return $this->redirectToRoute('pig_play');
-    }
-
-    #[Route("/game/pig/save", name: "pig_save", methods: ['POST'])]
-    public function save(
-        SessionInterface $session
-    ): Response
-    {
-        $roundTotal = $session->get("pig_round");
-        $gameTotal = $session->get("pig_total");
-
-        $session->set("pig_round", 0);
-        $session->set("pig_total", $roundTotal + $gameTotal);
-
         $this->addFlash(
-            'notice',
-            'Your round was saved to the total!'
-        );
-
-        return $this->redirectToRoute('pig_play');
+                        'notice',
+                        'Sessionen är raderad.'
+                        );
+        return $this->redirectToRoute('session');
     }
+
+    #[Route("/card/deck", name: "deck")]
+    public function deck(SessionInterface $session): Response
+    {
+        if (!$session->has("deck")) {
+            $session->set("deck", New DeckOfCards());    
+        }
+
+        $deck = $session->get("deck")->getString();
+        sort($deck);
+
+        $data = [
+            "deck" => $deck
+        ];
+        
+        return $this->render('card/deck.html.twig', $data);
+    }
+
+
+    // #[Route("/game/pig", name: "pig_start")]
+    // public function home(): Response
+    // {
+    //     return $this->render('pig/home.html.twig');
+    // }
+
+    // #[Route("/game/pig/test/roll", name: "test_roll_dice")]
+    // public function testRollDice(): Response
+    // {
+    //     $die = new Dice();
+
+    //     $data = [
+    //         "dice" => $die->roll(),
+    //         "diceString" => $die->getAsString(),
+    //     ];
+
+    //     return $this->render('pig/test/roll.html.twig', $data);
+    // }
+    // // <\d+> is a regular expression that specifies the format of the parameter. In this case, \d+ means one or more digits, so the num parameter must be a number.
+    // #[Route("/game/pig/test/roll/{num<\d+>}", name: "test_roll_num_dices")]
+    // public function testRollDices(int $num): Response
+    // {
+    //     if ($num > 99) {
+    //         throw new \Exception("Can not roll more than 99 dices!");
+    //     }
+
+    //     $diceRoll = [];
+    //     for ($i = 1; $i <= $num; $i++) {
+    //         $die = new DiceGraphic();
+    //         $die->roll();
+    //         $diceRoll[] = $die->getAsString();
+    //     }
+
+    //     $data = [
+    //         "num_dices" => count($diceRoll),
+    //         "diceRoll" => $diceRoll,
+    //     ];
+
+    //     return $this->render('pig/test/roll_many.html.twig', $data);
+    // }
+
+
+    // #[Route("/game/pig/test/dicehand/{num<\d+>}", name: "test_dicehand")]
+    // public function testDiceHand(int $num): Response
+    // {
+    //     if ($num > 99) {
+    //         throw new \Exception("Can not roll more than 99 dices!");
+    //     }
+
+    //     $hand = new DiceHand();
+    //     for ($i = 1; $i <= $num; $i++) {
+    //         if ($i % 2 === 1) {
+    //             $hand->add(new DiceGraphic());
+    //         } else {
+    //             $hand->add(new Dice());
+    //         }
+    //     }
+
+    //     $hand->roll();
+
+    //     $data = [
+    //         "num_dices" => $hand->getNumberDices(),
+    //         "diceRoll" => $hand->getString(),
+    //     ];
+
+    //     return $this->render('pig/test/dicehand.html.twig', $data);
+    // }
+
+    // #[Route("/game/pig/init", name: "pig_init_get", methods: ['GET'])]
+    // public function init(): Response
+    // {
+    //     return $this->render('pig/init.html.twig');
+    // }
+
+
+    // #[Route("/game/pig/init", name: "pig_init_post", methods: ['POST'])]
+    // public function initCallback(
+    //     Request $request,
+    //     SessionInterface $session
+    // ): Response
+    // {
+    //     $numDice = $request->request->get('num_dices');
+
+    //     $hand = new DiceHand();
+    //     for ($i = 1; $i <= $numDice; $i++) {
+    //         $hand->add(new DiceGraphic());
+    //     }
+    //     $hand->roll();
+
+    //     $session->set("pig_dicehand", $hand);
+    //     $session->set("pig_dices", $numDice);
+    //     $session->set("pig_round", 0);
+    //     $session->set("pig_total", 0);
+
+    //     return $this->redirectToRoute('pig_play');
+    // }
+
+    // #[Route("/game/pig/play", name: "pig_play", methods: ['GET'])]
+    // public function play(
+    //     SessionInterface $session
+    // ): Response
+    // {
+    //     $dicehand = $session->get("pig_dicehand");
+
+    //     $data = [
+    //         "pigDices" => $session->get("pig_dices"),
+    //         "pigRound" => $session->get("pig_round"),
+    //         "pigTotal" => $session->get("pig_total"),
+    //         "diceValues" => $dicehand->getString() 
+    //     ];
+
+    //     return $this->render('pig/play.html.twig', $data);
+    // }
+
+    // #[Route("/game/pig/roll", name: "pig_roll", methods: ['POST'])]
+    // public function roll(
+    //     SessionInterface $session
+    // ): Response
+    // {
+    //     $hand = $session->get("pig_dicehand");
+    //     $hand->roll();
+
+    //     $roundTotal = $session->get("pig_round");
+    //     $round = 0;
+    //     $values = $hand->getValues();
+    //     foreach ($values as $value) {
+    //         if ($value === 1) {
+    //             $round = 0;
+    //             $roundTotal = 0;
+    //             $this->addFlash(
+    //                 'warning',
+    //                 'You got a 1 and you lost the round points!'
+    //             );
+    //             break;
+    //         }
+    //         $round += $value;
+    //     }
+
+    //     $session->set("pig_round", $roundTotal + $round);
+        
+    //     return $this->redirectToRoute('pig_play');
+    // }
+
+    // #[Route("/game/pig/save", name: "pig_save", methods: ['POST'])]
+    // public function save(
+    //     SessionInterface $session
+    // ): Response
+    // {
+    //     $roundTotal = $session->get("pig_round");
+    //     $gameTotal = $session->get("pig_total");
+
+    //     $session->set("pig_round", 0);
+    //     $session->set("pig_total", $roundTotal + $gameTotal);
+
+    //     $this->addFlash(
+    //         'notice',
+    //         'Your round was saved to the total!'
+    //     );
+
+    //     return $this->redirectToRoute('pig_play');
+    // }
 
 }
