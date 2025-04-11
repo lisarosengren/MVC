9507ce1362ec4bffaf10cd4101cee164ffef67c3
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-04-12 00:20:01 +0200

    Add JSON controller

---
+++ src/Controller/DeckControllerJson.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\DeckOfCards;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class DeckControllerJson
+{
+    #[Route("/api/deck", name: "api_deck", methods: ['GET'])]
+    public function jsonDeck(SessionInterface $session): Response
+    {
+        if (!$session->has("deck")) {
+            $session->set("deck", New DeckOfCards());    
+        }
+
+        $deck = $session->get("deck")->getValues();
+        sort($deck);
+
+        $data = $deck;
+
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
