c2889b42067d16c31bbebdfa977f887296223bfe
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-05-06 12:23:33 +0200

    Create testCreateDeckOfCards. Verify that objects created has the right properties.

--- tests/Card/DeckOfCardsTest copy.php
+++ tests/Card/DeckOfCardsTest copy.php
@@ -35,116 +35,119 @@ class Game21Test extends TestCase
      */
     public function testCreateDeckOfCards(): void
     {
-        // $deck = new DeckOfCards();
-        $this->assertInstanceOf("\App\Card\DeckOfCards", $this->deck);
+        $this->assertInstanceOf("\App\Card\Game21", $this->game);
+        $this->assertObjectHasProperty("participants", $this->game);
+        $this->assertObjectHasProperty("gameStatus", $this->game);
+        $this->assertObjectHasProperty("deck", $this->game);
+        $this->assertObjectHasProperty("player", $this->game);
+        $this->assertObjectHasProperty("bank", $this->game);
 
-        $res = $this->deck->getValues();
-        $this->assertNotEmpty($res);
-    }
-
-    /**
-     * Verify that the shuffle method shuffles the cards.
-     */
-    public function testShuffle(): void
-    {
-        $notExp = $this->deck->getValues();
-
-        $this->deck->shuffleDeck();
-        $res = $this->deck->getValues();
-
-        $this->assertNotEquals($notExp, $res);
-    }
-
-
-    /**
-     * Verify that getSortedValues returns a string with cards sorted.
-     */
-    public function testGetSortedValues(): void
-    {
-        $sortedDeck = new DeckOfCards();
-        $exp = $sortedDeck->getValues();
-
-        $this->deck->shuffleDeck();
-        $res = $this->deck->getSortedValues();
-
-        $this->assertEquals($exp, $res);
-    }
-
-    /**
-     * Verify that numberOfCards returns the right number.
-     */
-    public function testNumberOfCards(): void
-    {
-        // Test with full deck of cards
-        $res = $this->deck->numberOfCards();
-        $this->assertEquals(52, $res);
-    }
-
-
-    /**
-     * Verify that drawCard returns the cards value as a string
-     * with utf-8 representation of card
-     * and that the last card is removed from the deck.
-     */
-    public function testdrawCard(): void
-    {
-        $res = $this->deck->drawCard();
-        $this->assertEquals("<div class='red'>&#127182</div>", $res);
-
-        // Test if there's no cards left
-        for ($i = 1; $i <= 51; $i++) {
-            $this->deck->drawCard();
-        }
-
-        $res = $this->deck->drawCard();
-        $this->assertEquals("No cards to draw", $res);
 
     }
 
-    /**
-     * Verify that drawCardJson returns the cards value as a string
-     * and that the last card is removed from the deck.
-     */
-    public function testdrawCardJson(): void
-    {
-        $res = $this->deck->drawCardJson();
-        $this->assertEquals("King of Diamonds", $res);
+//     /**
+//      * Verify that the shuffle method shuffles the cards.
+//      */
+//     public function testShuffle(): void
+//     {
+//         $notExp = $this->deck->getValues();
 
-        // Test if there's no cards left
-        for ($i = 1; $i <= 51; $i++) {
-            $this->deck->drawCard();
-        }
+//         $this->deck->shuffleDeck();
+//         $res = $this->deck->getValues();
 
-        $res = $this->deck->drawCardJson();
-        $this->assertEquals("No cards to draw", $res);
-    }
-
-    /**
-     * Verify that getString returns all the cards value as an array
-     * with strings with utf-8 representations
-     */
-    public function testGetString(): void
-    {
-        $res = $this->deck->getString();
-        $this->assertContains("<div class='black'>&#127187</div>", $res);
-    }
+//         $this->assertNotEquals($notExp, $res);
+//     }
 
 
-    /**
-     * Verify that drawCardGame returns the card that is being
-     * drawn and that it throws an exception is there's no cards left.
-     */
-    public function testdrawCardGame(): void
-    {
-        $res = $this->deck->drawCardGame();
-        $this->assertInstanceOf("\App\Card\CardGraphic", $res);
+//     /**
+//      * Verify that getSortedValues returns a string with cards sorted.
+//      */
+//     public function testGetSortedValues(): void
+//     {
+//         $sortedDeck = new DeckOfCards();
+//         $exp = $sortedDeck->getValues();
 
-        // Test if there's no cards left
-        for ($i = 1; $i <= 51; $i++) {
-            $this->deck->drawCardGame();
-        }
-        $this->expectException(Exception::class);
-        $res = $this->deck->drawCardGame();
+//         $this->deck->shuffleDeck();
+//         $res = $this->deck->getSortedValues();
 
-    }
+//         $this->assertEquals($exp, $res);
+//     }
+
+//     /**
+//      * Verify that numberOfCards returns the right number.
+//      */
+//     public function testNumberOfCards(): void
+//     {
+//         // Test with full deck of cards
+//         $res = $this->deck->numberOfCards();
+//         $this->assertEquals(52, $res);
+//     }
+
+
+//     /**
+//      * Verify that drawCard returns the cards value as a string
+//      * with utf-8 representation of card
+//      * and that the last card is removed from the deck.
+//      */
+//     public function testdrawCard(): void
+//     {
+//         $res = $this->deck->drawCard();
+//         $this->assertEquals("<div class='red'>&#127182</div>", $res);
+
+//         // Test if there's no cards left
+//         for ($i = 1; $i <= 51; $i++) {
+//             $this->deck->drawCard();
+//         }
+
+//         $res = $this->deck->drawCard();
+//         $this->assertEquals("No cards to draw", $res);
+
+//     }
+
+//     /**
+//      * Verify that drawCardJson returns the cards value as a string
+//      * and that the last card is removed from the deck.
+//      */
+//     public function testdrawCardJson(): void
+//     {
+//         $res = $this->deck->drawCardJson();
+//         $this->assertEquals("King of Diamonds", $res);
+
+//         // Test if there's no cards left
+//         for ($i = 1; $i <= 51; $i++) {
+//             $this->deck->drawCard();
+//         }
+
+//         $res = $this->deck->drawCardJson();
+//         $this->assertEquals("No cards to draw", $res);
+//     }
+
+//     /**
+//      * Verify that getString returns all the cards value as an array
+//      * with strings with utf-8 representations
+//      */
+//     public function testGetString(): void
+//     {
+//         $res = $this->deck->getString();
+//         $this->assertContains("<div class='black'>&#127187</div>", $res);
+//     }
+
+
+//     /**
+//      * Verify that drawCardGame returns the card that is being
+//      * drawn and that it throws an exception is there's no cards left.
+//      */
+//     public function testdrawCardGame(): void
+//     {
+//         $res = $this->deck->drawCardGame();
+//         $this->assertInstanceOf("\App\Card\CardGraphic", $res);
+
+//         // Test if there's no cards left
+//         for ($i = 1; $i <= 51; $i++) {
+//             $this->deck->drawCardGame();
+//         }
+//         $this->expectException(Exception::class);
+//         $res = $this->deck->drawCardGame();
+
+//     }
 }
