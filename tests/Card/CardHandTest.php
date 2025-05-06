fd589c9c03b19f9efb0845ff958110244190d6ef
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-05-06 10:48:19 +0200

    Write tests for CardHand - 100% coverage.

---
+++ tests/Card/CardHandTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\CardGraphic;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Card.
+ */
+class CardHandTest extends TestCase
+{
+    private ?CardHand $hand;
+
+    /**
+     * Create new hand for the tests.
+     */
+    protected function setUp(): void
+    {
+        $this->hand = new CardHand();
+    }
+
+
+    /**
+     * Erase the new hand after the tests.
+     */
+    protected function tearDown(): void
+    {
+        $this->hand = null;
+    }
+
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testCreateCardHand(): void
+    {
+        $this->assertInstanceOf("\App\Card\CardHand", $this->hand);
+    }
+
+    /**
+     * Add cards to the hand and verify their existence
+     */
+    public function testAdd(): void
+    {
+        $this->hand->add(new CardGraphic("Ace of Spades"));
+        $this->hand->add(new CardGraphic("Queen of Clubs"));
+        $res = count($this->hand->getValues());
+
+        $this->assertEquals(2, $res);
+    }
+
+    /**
+     * Add cards to the hand and verify that getString
+     * returns and array with the cards value.
+     */
+    public function testGetString(): void
+    {
+        $this->hand->add(new CardGraphic("Ace of Spades"));
+        $this->hand->add(new CardGraphic("Queen of Clubs"));
+        $res = ($this->hand->getString());
+
+        $this->assertContains("<div class='black'>&#127137</div>", $res);
+    }
+
+}
