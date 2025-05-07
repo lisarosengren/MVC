f2c7ae9ef66d107739e74a8ecad8b44ff53c556a
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-05-07 18:35:27 +0200

    Create tests for banksTurn.

--- tests/Card/Game21Test.php
+++ tests/Card/Game21Test.php
@@ -133,26 +133,84 @@ class Game21Test extends TestCase
     }
 
     /**
-     * Verify that Winner updates gameStatus with the right value.
+     * Verify that banksTurn and winner method updates the gameStatus correct.
      */
-    // public function testWinner(): void
-    // {
-    //     // When bank won.
-    //     $this->game->firstDraw("bank");
-    //     $res = $this->game->winner();
-    //     $res = $this->game->getStatus();
-    //     $this->assertEquals("Banken vann!", $res);
+    public function testBanksTurn(): void
+    {
+        $deck = new DeckOfCards();
+        // Create stubs for the CardHand class.
+        $stubPlayer = $this->createMock(CardHand::class);
+        $stubPlayer2 = $this->createMock(CardHand::class);
+        $stubBank1 = $this->createMock(CardHand::class);
+        $stubBank2 = $this->createMock(CardHand::class);
+        $stubBank3 = $this->createMock(CardHand::class);
+        $stubBank4 = $this->createMock(CardHand::class);
 
-    //     // When player won.
-    //     $this->game->firstDraw("player");
-    //     $res = $this->game->winner();
-    //     $res = $this->game->getStatus();
-    //     $this->assertEquals("Spelaren vann!", $res);
-    // }
+        // Configure the stubs.
+        $stubPlayer->method('getValues')
+                ->willReturn(["Two of Spades", "Two of Clubs"]);
+        $stubBank1->method('getValues')
+                ->willReturn(["King of Spades", "King of Clubs"]);
+        $stubBank2->method('getValues')
+                ->willReturn(["Jack of Spades", "Ten of Clubs"]);
+        $stubBank3->method('getValues')
+                ->willReturn(["Ten of Spades", "Ten of Clubs"]);
+        $stubPlayer2->method('getValues')
+                ->willReturn(["Jack of Spades", "Ten of Clubs"]);
 
 
+        // Verify that player is winner when bank is over 21.
+        $stubGame = new Game21($stubPlayer, $stubBank1, $deck);
+        $stubGame->banksTurn();
+        $res = $stubGame->getStatus();
+        $this->assertEquals("Spelaren vann!", $res);
+
+        // Verify that bank is winner when bank has 21.
+        $stubGame2 = new Game21($stubPlayer2, $stubBank2, $deck);
+        $stubGame->calculateSum("player");
+        $stubGame2->banksTurn();
+        $res = $stubGame2->getStatus();
+        $this->assertEquals("Banken vann!", $res);
+
+        // Verify that bank is winner when bank is closer
+        // than player to 21.
+        $stubGame = new Game21($stubPlayer, $stubBank3, $deck);
+        $stubGame->calculateSum("player");
+        $stubGame->banksTurn();
+        $res = $stubGame->getStatus();
+        $this->assertEquals("Banken vann!", $res);
+
+        // Verify that player is winner when player is closer
+        // than bank to 21.
+        $stubGame = new Game21($stubPlayer2, $stubBank3, $deck);
+        $stubGame->calculateSum("player");
+        $stubGame->banksTurn();
+        $res = $stubGame->getStatus();
+        $this->assertEquals("Spelaren vann!", $res);
+    }
 
 
+    /**
+     * Verify that banksTurn calls draw when score less than 17.
+     * Made by checking that exception is raised (empty deck).
+     */
+    public function testBanksTurnLess(): void
+    {
+        $deck = new DeckOfCards();
+        // Create stubs for the CardHand class.
+        $stubPlayer = $this->createMock(CardHand::class);
+        $stubBank = $this->createMock(CardHand::class);
+
+        // Configure the stubs.
+        $stubPlayer->method('getValues')
+                ->willReturn(["Two of Spades", "Two of Clubs"]);
+        $stubBank->method('getValues')
+                ->willReturn(["Three of Spades", "Three of Clubs"]);
+                
+        $stubGame = new Game21($stubPlayer, $stubBank, $deck);
+        $this->expectException(Exception::class);
+        $stubGame->banksTurn();
+    }
 
 
 //     /**
