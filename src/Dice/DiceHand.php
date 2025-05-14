bd29ac25c7410705b58195336b2f991f9fd798c6
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2025-05-14 17:32:49 +0200

    Comment out the Dice classes

--- src/Dice/DiceHand.php
+++ src/Dice/DiceHand.php
@@ -1,64 +1,64 @@
 <?php
 
-namespace App\Dice;
+// namespace App\Dice;
 
-use App\Dice\Dice;
+// use App\Dice\Dice;
 
-class DiceHand
-{   /**
-    * @var array<Dice>
-    */
-    private array $hand = [];
+// class DiceHand
+// {   /**
+//     * @var array<Dice>
+//     */
+//     private array $hand = [];
 
-    public function add(Dice $die): void
-    {
-        $this->hand[] = $die;
-    }
+//     public function add(Dice $die): void
+//     {
+//         $this->hand[] = $die;
+//     }
 
-    public function roll(): void
-    {
-        foreach ($this->hand as $die) {
-            $die->roll();
-        }
-    }
+//     public function roll(): void
+//     {
+//         foreach ($this->hand as $die) {
+//             $die->roll();
+//         }
+//     }
 
-    public function getNumberDices(): int
-    {
-        return count($this->hand);
-    }
-    /**
-     * @return array<int>
-     */
-    public function getValues(): array
-    {
-        $values = [];
-        foreach ($this->hand as $die) {
-            $values[] = $die->getValue();
-        }
-        return $values;
-    }
+//     public function getNumberDices(): int
+//     {
+//         return count($this->hand);
+//     }
+//     /**
+//      * @return array<int>
+//      */
+//     public function getValues(): array
+//     {
+//         $values = [];
+//         foreach ($this->hand as $die) {
+//             $values[] = $die->getValue();
+//         }
+//         return $values;
+//     }
 
-    /**
-     * @return array<string>
-     */
-    public function getString(): array
-    {
-        $values = [];
-        foreach ($this->hand as $die) {
-            $values[] = $die->getAsString();
-        }
-        return $values;
-    }
+//     /**
+//      * @return array<string>
+//      */
+//     public function getString(): array
+//     {
+//         $values = [];
+//         foreach ($this->hand as $die) {
+//             $values[] = $die->getAsString();
+//         }
+//         return $values;
+//     }
 
-    /**
-     * @return int
-     */
-    public function sum(): int
-    {
-        $sum = 0;
-        foreach ($this->hand as $die) {
-            $sum += $die->getValue();
-        }
-        return $sum;
-    }
-}
+//     /**
+//      * @return int
+//      */
+//     public function sum(): int
+//     {
+//         $sum = 0;
+//         foreach ($this->hand as $die) {
+//             $sum += $die->getValue();
+//         }
+//         return $sum;
+//     }
+// }
